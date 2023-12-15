35941f06937295c932c8afbdc2d7195b0108afdc
Pedro <prlara2000@gmail.com>
2023-12-15 16:11:21 -0600

    talwinds and icons

--- tailwind/src/App.tsx
+++ tailwind/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
 import { WiEarthquake } from 'react-icons/wi';
+
 function App() {
   const [count, setCount] = useState(0);
 
